69adcc595be82a9041d4e5b0908ecc2b8ecc3734
Mattia Paolacci <mat.paolacci@gmail.com>
2024-09-30 19:38:44 +0200

    .gitignore

--- dataset-utilities/main.py
+++
@@ -1,9 +0,0 @@
-import flacconverter as fc
-import datasetanalyzer as da
-from datasetanalyzer import InstrumentType as Instr
-import os
-
-if __name__ == "__main__":
-    da.loadConfigPathsYaml(os.path.join(os.getcwd(),"dataset-utilities" , "configPaths.yaml"))
-    da.buildDatasetZip({'Piano', 'Drums', 'Guitar', 'Bass'}, 0.20)
-    #da.buildInstrumentsUsageFrequencyHistogram(da.SetType.TRAIN)
